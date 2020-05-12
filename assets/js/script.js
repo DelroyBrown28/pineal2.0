93ef0776e39a4fbbf6bf1a717f3502193c94de9b
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-12 14:17:14 +0100

    added jquery animation for art vault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,54 @@
 $(document).ready(function () {
 
+    $(".art_vault_title").click(hideMainElements);
 
-    $('.quote_carousel').slick({
-        autoplay: true,
-        speed: 10000,
-        dots: false,
-        infinite: true,
-    });
+    function hideMainElements() {
+        $(".art_vault_title").animate({
+            color: "black",
+            bottom: "20%",
+        }, 800, "easeInOutCubic");
+        $(".forbidden_unknown_title").delay(500).animate({
+            color: "black",
+            opacity: "0",
+            bottom: "500%",
+        }, 3000, "easeInOutCubic")
+        $(".media_news_title").delay(600).animate({
+            color: "black",
+            opacity: "0",
+            bottom: "500%",
+        }, 3000, "easeInOutCubic")
+        $(".podcasts_music").delay(800).animate({
+            color: "black",
+            opacity: "0",
+            bottom: "500%",
+        }, 3000, "easeInOutCubic")
+        $(".main_blurb").delay(800).animate({
+            color: "black",
+            opacity: "0",
+        }, 3000, "easeInOutCubic")
+        $(".socials").delay(1000).animate({
+            color: "black",
+            opacity: "0",
+            bottom: "500%",
+        }, 3000, "easeInOutCubic")
+        $(".github").delay(1200).animate({
+            color: "black",
+            opacity: "0",
+            bottom: "500%",
+        }, 3000, "easeInOutCubic")
+
+}
+
+
+// QUOTES CAROUSEL
+$('.quote_carousel').slick({
+    autoplay: true,
+    speed: 10000,
+    dots: false,
+    infinite: true,
+});
+
+
 
 
 });
